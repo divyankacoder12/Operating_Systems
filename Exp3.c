ebe46712d843475901f58431055d9747e17ace78
divyankacoder12 <divyankasagolale@gmail.com>
2026-01-27 11:46:26 +0530

    Experiment 3

---
+++ Exp3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+ int a = 10;
+ int b = 20;
+ int result = a+b;
+ printf("sum:%d\n",result);
+
+ int diff = a - b;
+printf("diff:%d\n",diff);
+
+int product = a*b;
+printf("product:%d\n",product);
+
+int div = a/b;
+printf("division:%d\n",div);
+
+ return 0;
+}
