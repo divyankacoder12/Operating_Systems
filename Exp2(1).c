15de732925fe5d601e8107807592c119c71047a6
divyankacoder12 <divyankasagolale@gmail.com>
2026-01-27 11:43:17 +0530

    Experiment 2

---
+++ Exp2(1).c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+ int main(){
+  int leap_yr;
+ printf("enter a year:");
+ scanf("%d",&leap_yr);
+ if(leap_yr % 4 == 0){
+
+  printf("It is a leap year");
+
+}else{
+
+
+ printf("It isnot a leap year"); 
+}
+
+}
---
+++ Exp2.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+int main(){
+ int a;
+ printf("enter a: ");
+scanf("%d",&a);
+if(a % 2 == 0){ 
+
+printf("Even/n");
+}else{
+
+printf("not even"); 
+}
+
+if(a%2!=0){
+printf("Odd");
+}else{
+printf("not odd");
+}
+
+}
